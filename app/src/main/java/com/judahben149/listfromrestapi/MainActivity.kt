18a4d265c3ec2f0d7e3b88fab6a0a9c9682416e0
Judah <judahben149@gmail.com>
2022-02-03 03:22:07 +0100

    Update MainActivity.kt

--- app/src/main/java/com/judahben149/listfromrestapi/MainActivity.kt
+++ app/src/main/java/com/judahben149/listfromrestapi/MainActivity.kt
@@ -3,10 +3,9 @@ package com.judahben149.listfromrestapi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.judahben149.listfromrestapi.databinding.ActivityMainBinding
-import com.judahben149.listfromrestapi.model.PostItem
+import com.judahben149.listfromrestapi.model.GitHubUser
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -17,62 +16,56 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var rvList: RecyclerView
-    private lateinit var myAdapter: ListAdapter
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val post: List<PostItem>
 
-
-//        rvList = binding.rvList
-//
-//        rvList.apply {
-//            layoutManager = LinearLayoutManager(this@MainActivity)
-//        }
-
-//        retrofitAndApiStuff()
-
-        binding
         fetchGitHubUserDetails()
     }
 
-    private fun fetchGitHubUserDetails(userName: String) {
+    private fun fetchGitHubUserDetails() {
         val retrofitGitHubAPI = Retrofit.Builder()
-            .baseUrl("https://api.github.com/users/")
+            .baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
 
-        retrofitGitHubAPI.getUserDetails(userName)
-    }
+        binding.btnFetchUserDetails.setOnClickListener {
+            val userName = binding.etEnterUserName.text.toString()
+            Snackbar.make(
+                binding.root,
+                userName,
+                Snackbar.LENGTH_LONG
+            ).show()
 
-//    private fun retrofitAndApiStuff() {
-//        val retrofit = Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//
-//        val api = retrofit.create(ApiService::class.java)
-//
-//        api.fetchAllPosts().enqueue(object: Callback<List<PostItem>>{
-//            override fun onResponse(call: Call<List<PostItem>>, response: Response<List<PostItem>>) {
-//                val responseBody = response.body()!!
-//
-//                myAdapter = ListAdapter(baseContext, responseBody)
-//                myAdapter.notifyDataSetChanged()
-//                rvList.adapter = myAdapter
-//
-//
-//            }
-//
-//            override fun onFailure(call: Call<List<PostItem>>, t: Throwable) {
-//                Log.d("judahActivity", "onFailure")
-//            }
-//
-//        })
-//    }
+            retrofitGitHubAPI.getUserDetails(userName).enqueue(object : Callback<GitHubUser> {
+                override fun onResponse(call: Call<GitHubUser>, response: Response<GitHubUser>) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        Log.d("MainActivity", responseBody.followers.toString())
+                    }
+                    Snackbar.make(
+                        binding.root,
+                        responseBody?.login.toString(),
+                        Snackbar.LENGTH_LONG
+                    ).show()
+
+
+                    binding.tvUserFullName.text = responseBody?.name
+                    binding.tvUserName.text = responseBody?.login
+                    binding.tvUserLocation.text = responseBody?.location
+                }
+
+                override fun onFailure(call: Call<GitHubUser?>, t: Throwable) {
+                    Snackbar.make(
+                        binding.root,
+                        "Error fetching details - " + t.message,
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            })
+        }
+    }
 }
